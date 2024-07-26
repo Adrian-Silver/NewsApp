ab96915e6bc8b7edb667b16172008230c323063e
Adrian-Silver <adrianallangitau@gmail.com>
2024-07-26 14:58:24 +0300

    Add network and data DI in NewsAppModule

--- app/src/main/java/com/example/newsapp/di/NewsAppModule.kt
+++ app/src/main/java/com/example/newsapp/di/NewsAppModule.kt
@@ -1,16 +1,27 @@
 package com.example.newsapp.di
 
 import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.newsapp.data.local.NewsDao
+import com.example.newsapp.data.local.NewsDatabase
+import com.example.newsapp.data.local.converters.NewsTypeConverter
 import com.example.newsapp.data.manager.LocalUserManagerImpl
+import com.example.newsapp.data.remote.NewsApi
+import com.example.newsapp.data.repository.NewsRepositoryImpl
 import com.example.newsapp.domain.manager.LocalUserManager
+import com.example.newsapp.domain.repository.NewsRepository
 import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 
@@ -21,6 +32,7 @@ object NewsAppModule {
     @Provides
     @Singleton
     fun provideLocalUserManager(
+//        @ApplicationContext context: Context
         application: Application
     ): LocalUserManager = LocalUserManagerImpl(context = application)
 
@@ -32,4 +44,46 @@ object NewsAppModule {
         readAppEntry = ReadAppEntry(localUserManager),
         saveAppEntry = SaveAppEntry(localUserManager)
     )
+
+    @Provides
+    @Singleton
+    fun provideApiInstance(): NewsApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(
+        application: Application
+    ): NewsDatabase {
+        return Room.databaseBuilder(
+            context = application,
+            klass = NewsDatabase::class.java,
+            name = "news_db"
+        ).addTypeConverter(NewsTypeConverter())
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsDao(
+        newsDatabase: NewsDatabase
+    ): NewsDao = newsDatabase.newsDao
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi,
+        newsDao: NewsDao
+    ): NewsRepository {
+        return NewsRepositoryImpl(newsApi, newsDao)
+    }
+
+
 }

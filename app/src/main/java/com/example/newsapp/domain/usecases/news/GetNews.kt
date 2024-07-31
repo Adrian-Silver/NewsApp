35ca1956e220883265fdec20ae671e7ea6a90c8e
Adrian-Silver <adrianallangitau@gmail.com>
2024-08-01 01:23:57 +0300

    Change constructor parameter from newsRepositoryImpl to newsRepository

--- app/src/main/java/com/example/newsapp/domain/usecases/news/GetNews.kt
+++ app/src/main/java/com/example/newsapp/domain/usecases/news/GetNews.kt
@@ -3,12 +3,15 @@ package com.example.newsapp.domain.usecases.news
 import androidx.paging.PagingData
 import com.example.newsapp.data.local.ArticleEntity
 import com.example.newsapp.data.repository.NewsRepositoryImpl
+import com.example.newsapp.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetNews(
-    private val newsRepositoryImpl: NewsRepositoryImpl
+//    private val newsRepositoryImpl: NewsRepositoryImpl
+    private val newsRepository: NewsRepository
 ) {
     operator fun invoke(sources: List<String>): Flow<PagingData<ArticleEntity>> {
-        return newsRepositoryImpl.getNews(sources = sources)
+//        return newsRepositoryImpl.getNews(sources = sources)
+        return newsRepository.getNews(sources = sources)
     }
 }
--- app/src/main/java/com/example/newsapp/domain/usecases/news/SearchNews.kt
+++ app/src/main/java/com/example/newsapp/domain/usecases/news/SearchNews.kt
@@ -3,13 +3,19 @@ package com.example.newsapp.domain.usecases.news
 import androidx.paging.PagingData
 import com.example.newsapp.data.local.ArticleEntity
 import com.example.newsapp.data.repository.NewsRepositoryImpl
+import com.example.newsapp.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
 
 class SearchNews(
-    private val newsRepositoryImpl: NewsRepositoryImpl
+//    private val newsRepositoryImpl: NewsRepositoryImpl
+    private val newsRepository: NewsRepository
 ) {
     operator fun invoke(searchQuery: String, sources: List<String>): Flow<PagingData<ArticleEntity>> {
-        return newsRepositoryImpl.searchNews(
+//        return newsRepositoryImpl.searchNews(
+//            searchQuery = searchQuery,
+//            sources = sources
+//        )
+        return newsRepository.searchNews(
             searchQuery = searchQuery,
             sources = sources
         )

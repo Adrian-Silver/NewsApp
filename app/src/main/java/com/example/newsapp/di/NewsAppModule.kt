cf9f8c7d5fe650c57b25db6a1872a80e7d1c2bcd
Adrian-Silver <adrianallangitau@gmail.com>
2024-08-01 01:25:48 +0300

    Add provideNewsCases DI

--- app/src/main/java/com/example/newsapp/di/NewsAppModule.kt
+++ app/src/main/java/com/example/newsapp/di/NewsAppModule.kt
@@ -14,12 +14,21 @@ import com.example.newsapp.domain.repository.NewsRepository
 import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.domain.usecases.news.AddBookmark
+import com.example.newsapp.domain.usecases.news.DeleteBookmark
+import com.example.newsapp.domain.usecases.news.GetArticle
+import com.example.newsapp.domain.usecases.news.GetArticles
+import com.example.newsapp.domain.usecases.news.GetNews
+import com.example.newsapp.domain.usecases.news.NewsUseCases
+import com.example.newsapp.domain.usecases.news.SearchNews
 import com.example.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -48,9 +57,18 @@ object NewsAppModule {
     @Provides
     @Singleton
     fun provideApiInstance(): NewsApi {
+
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         return Retrofit
             .Builder()
             .baseUrl(BASE_URL)
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(NewsApi::class.java)
@@ -83,6 +101,22 @@ object NewsAppModule {
         newsDao: NewsDao
     ): NewsRepository {
         return NewsRepositoryImpl(newsApi, newsDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository,
+        newsDao: NewsDao
+    ): NewsUseCases {
+        return NewsUseCases(
+            getNews = GetNews(newsRepository),
+            searchNews = SearchNews(newsRepository),
+            addBookmark = AddBookmark(newsDao),
+            deleteBookmark = DeleteBookmark(newsDao),
+            getArticle = GetArticle(newsDao),
+            getArticles = GetArticles(newsDao)
+        )
     }
 
 

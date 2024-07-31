1836aeec82f29585f806146db508bedc1e95f0c7
Adrian-Silver <adrianallangitau@gmail.com>
2024-08-01 01:32:09 +0300

    Add Dimens.kt

---
+++ app/src/main/java/com/example/newsapp/util/Dimens.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp.util
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    // Padding
+    // Small
+    val SmallPadding = 4.dp
+    val SmallPadding2 = 8.dp
+    // Medium
+    val MediumPadding = 24.dp
+    val MediumPadding2 = 32.dp
+    val MediumPadding3 = 40.dp
+
+    // Indicator
+    val IndicatorSize = 16.dp
+
+    // Icons
+    val SmallIconSize = 12.dp
+    val IconSize = 20.dp
+
+    // Article
+    val ArticleCardSize = 96.dp
+    val ArticleImageHeight = 248.dp
+
+}

bb301bc8e594c7d45b95648d2d9d21dfc31afd60
Adrian-Silver <adrianallangitau@gmail.com>
2024-08-01 01:43:04 +0300

    Add navigation

---
+++ app/src/main/java/com/example/newsapp/presenetation/navigation/news_navigator/components/BottomNavigationItem.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.presenetation.navigation.news_navigator.components
+
+import androidx.annotation.DrawableRes
+
+data class BottomNavigationItem(
+    @DrawableRes val icon: Int,
+    val text: String
+)
